9104c62ccbdd6a5711106a2bd8c800cf7dfc587b
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-10-27 01:37:25 +0300

    completed assgnment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+class Dancer
+    include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+class Dancer
+    extend MetaDancing
+  end
+  class Dancer
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,23 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+
+    include Dance
+
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+end
+
+class Kid
+    extend MetaDancing
+  end
+
+  class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  end
